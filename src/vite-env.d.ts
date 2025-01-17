37c86af9b5772f068a281587bc8342dbd3547162
Amit Kumar Gupta <amitgupta542173@gmail.com>
2025-01-17 09:46:49 +0530

    initial commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="vite/client" />
+// this is the typescript referencee file used fo the default vite typescript setup and given the boilerplate like thing for the react and typescript . 
